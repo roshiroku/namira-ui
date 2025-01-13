6b3a76fefd982cf574d38cba66730056a071b29e
roshiroku <shir.oraan@gmail.com>
2025-01-13 23:55:52 +0200

    update typography

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,19 +1,20 @@
 import { FC, PropsWithChildren } from 'react';
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material';
+import '@fontsource-variable/inter/index.css';
 
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
   const theme = createTheme({
     palette: { mode: 'light' },
+    typography: {
+      fontFamily: "'Inter Variable', sans-serif",
+      h6: { lineHeight: 1.5 }
+    },
     components: {
       MuiAppBar: {
-        defaultProps: {
-          elevation: 0
-        }
+        defaultProps: { elevation: 0 }
       },
       MuiButton: {
-        defaultProps: {
-          disableElevation: true
-        }
+        defaultProps: { disableElevation: true }
       }
     }
   });
