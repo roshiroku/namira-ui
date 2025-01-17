349fdcb2e5eeb00bf136840422b15169375903a3
roshiroku <shir.oraan@gmail.com>
2025-01-17 20:49:59 +0200

    update quality detection

--- src/providers/ImageProvider.tsx
+++ src/providers/ImageProvider.tsx
@@ -66,12 +66,11 @@ const ImageProvider: FC<PropsWithChildren> = ({ children }) => {
       const image = images[i];
       const progress = Math.round(((i + 1) / images.length) * 100);
 
-      setProgressDialog({ open: true, message: `Detecting quality for ${image.name} (${i + 1}/${images.length})`, progress });
-
       let { quality } = settings;
 
-      if (['image/jpg', 'image/jpeg', 'image/webp'].includes(format)) {
+      if (format !== 'image/png') {
         if (quality < 0 || quality > 1) {
+          setProgressDialog({ open: true, message: `Detecting quality for ${image.name} (${i + 1}/${images.length})`, progress });
           quality = await detectQuality(image, format);
         }
       } else {
