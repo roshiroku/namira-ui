e0f9acb7a66960e4525ee2b8e96fd7f4bae65529
roshiroku <shir.oraan@gmail.com>
2025-01-18 20:14:12 +0200

    remove strict mode in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  import.meta.env.PROD ? (
     <App />
-  </StrictMode>,
-)
+  ) : (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+);
